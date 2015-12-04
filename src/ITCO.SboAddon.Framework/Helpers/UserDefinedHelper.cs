f8c15ef9b4cc1a1593d7aec41c825457342b66b8
Kenny Westermark <kenny.westermark@itcompaniet.se>
2015-12-04 14:36:50 +0100

    UD Helper, Create Field/Table

--- src/ITCO.SboAddon.Framework/Helpers/UserDefinedHelper.cs
+++ src/ITCO.SboAddon.Framework/Helpers/UserDefinedHelper.cs
@@ -1,9 +1,78 @@
 ﻿using SAPbobsCOM;
+using System;
 
 namespace ITCO.SboAddon.Framework.Helpers
 {
     public class UserDefinedHelper
     {
+        public static bool CreateTable(string tableName, string tableDescription, BoFieldTypes type, int size = 30)
+        {
+            UserTablesMD userTablesMD = null;
+
+            try
+            {
+                userTablesMD = SboApp.Company.GetBusinessObject(BoObjectTypes.oUserTables) as UserTablesMD;
+
+                if (!userTablesMD.GetByKey(tableName))
+                {
+                    userTablesMD.TableName = tableName;
+                    userTablesMD.TableDescription = tableDescription;
+                    ErrorHelper.HandleErrorWithException(
+                        userTablesMD.Add(), 
+                        string.Format("Could not create UDT {0}", tableName));
+                }
+            }
+            catch (Exception ex)
+            {
+                SboApp.Application.MessageBox(ex.Message);
+                return false;
+            }
+            finally
+            {
+                if (userTablesMD != null)
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(userTablesMD);
+            }
+
+            return true;
+        }
+
+        public static bool CreateField(string tableName, string fieldName, string fieldDescription, BoFieldTypes type, int size = 30)
+        {
+            UserFieldsMD userFieldsMD = null;
+
+            try
+            {
+                userFieldsMD = SboApp.Company.GetBusinessObject(BoObjectTypes.oUserFields) as UserFieldsMD;
+
+                var fieldId = UserDefinedHelper.GetFieldId(tableName, fieldName);
+                if (fieldId == -1)
+                {
+                    userFieldsMD.TableName = tableName;
+                    userFieldsMD.Name = fieldName;
+                    userFieldsMD.Description = fieldDescription;
+                    userFieldsMD.Type = type;
+                    userFieldsMD.Size = size;
+                    userFieldsMD.EditSize = size;
+                    ErrorHelper.HandleErrorWithException(
+                        userFieldsMD.Add(), 
+                        string.Format("Could not create {0} on {1}", fieldName, tableName));
+                }
+            }
+            catch (Exception ex)
+            {
+                SboApp.Application.MessageBox(ex.Message);
+                return false;
+            }
+            finally
+            {
+                if (userFieldsMD != null)
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(userFieldsMD);
+            }
+
+            return true;
+        }
+
+
         public static int GetFieldId(string tableName, string fieldAlias)
         {
             var recordSet = SboApp.Company.GetBusinessObject(BoObjectTypes.BoRecordset) as Recordset;
