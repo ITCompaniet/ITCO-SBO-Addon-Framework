6dd220e6d177f6ca2582e0ef4040b94b2047a87e
Kenny Westermark <kenny.westermark@itcompaniet.se>
2015-12-04 15:12:16 +0100

    More SboForm

--- src/ITCO.SboAddon.Framework/Constants/SboForm.cs
+++ src/ITCO.SboAddon.Framework/Constants/SboForm.cs
@@ -2,9 +2,38 @@
 {
     public class SboForm
     {
-        public const string Invoice = "133";
-        public const string Order = "139";
+        // Financials
+        public const string JournalEntry = "392";
+
+        // Sales
+        public const string SalesQuotation = "149";
+        public const string SalesOrder = "139";
         public const string Delivery = "140";
-        public const string Quotation = "149";
+        public const string Returns = "180";
+        public const string ARDownPaymentInvoice = "65300";
+        public const string ARDownPaymentRequest = "65308";
+        public const string ARInvoice = "133";
+        public const string ARInvoiceAndPayment = "60090";
+        public const string ARCreditMemo = "179";
+
+        // Purchase
+        public const string PurchaseRequest = "1470000200";
+        public const string PurchaseQuotation = "540000988";
+        public const string PurchaseOrder = "142";
+        public const string GoodsReceipt = "143";
+        public const string GoodsReturns = "182";
+        public const string APInvoice = "141";
+
+        // Business Partners
+        public const string BusinessPartner = "134";
+        public const string Activity = "651";
+
+        // Inventory
+        public const string ItemMasterData = "150";
+        public const string PriceLists = "155";
+        public const string PriceList = "157";
+
+        // Production
+        public const string ProductionOrder = "65211";
     }
 }
