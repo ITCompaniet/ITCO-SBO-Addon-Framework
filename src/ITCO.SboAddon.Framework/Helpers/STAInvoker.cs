9625d680c67cc279c082ede938de61e8214b61be
Kenny Westermark <kenny.westermark@itcompaniet.se>
2015-12-14 17:35:17 +0100

    STAInvoker

---
+++ src/ITCO.SboAddon.Framework/Helpers/STAInvoker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace ITCO.SboAddon.Framework.Helpers
+{
+    /// <summary>
+    /// Invoice STATThread Methods
+    /// </summary>
+    /// <typeparam name="T">Object type</typeparam>
+    /// <typeparam name="TReturn">Return type</typeparam>
+    public class STAInvoker<T, TReturn>
+    {
+        private Thread _invokeThread;
+        private Func<T, TReturn> _invoker;
+        private TReturn _result;
+        private T _statObject;
+
+        /// <summary>
+        /// Initialize Invoker with objec and invoice method
+        /// </summary>
+        /// <param name="staTreadObject"></param>
+        /// <param name="invoker"></param>
+        public STAInvoker(T staTreadObject, Func<T, TReturn> invoker)
+        {
+            _statObject = staTreadObject;
+            _invoker = invoker;
+            _invokeThread = new Thread(new ThreadStart(InvokeMethod));
+            _invokeThread.SetApartmentState(ApartmentState.STA);
+        }
+        /// <summary>
+        /// Invoke STATThread method
+        /// </summary>
+        /// <returns>TReturn</returns>
+        public TReturn Invoke()
+        {
+            _invokeThread.Start();
+            _invokeThread.Join();
+            return _result;
+        }
+
+        private void InvokeMethod()
+        {
+            _result = _invoker.Invoke(_statObject);
+        }
+    }
+}
