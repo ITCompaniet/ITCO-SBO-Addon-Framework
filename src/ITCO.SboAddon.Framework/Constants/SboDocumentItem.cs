107d3f53151058d19a1f69a2b7425abe663096f7
Kenny Westermark <kenny.westermark@itcompaniet.se>
2016-01-14 16:35:36 +0100

    SboDocumentItem

---
+++ src/ITCO.SboAddon.Framework/Constants/SboDocumentItem.cs
@@ -0,0 +1,12 @@
+﻿namespace ITCO.SboAddon.Framework.Constants
+{
+    public static class SboDocumentItem
+    {
+        public const string CardCode = "4";
+        public const string Matrix = "38";
+        public const string MatrixItemCode = "1";
+        public const string MatrixQuantity = "11";
+        public const string MatrixUnitPrice = "14";
+        public const string MatrixWeight = "58";
+    }
+}
