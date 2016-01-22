ee740b913655c1040a14bb3bbace5d91fd8986b2
Kenny Westermark <kenny.westermark@itcompaniet.se>
2016-01-22 11:51:58 +0100

    SboTransaction

---
+++ src/ITCO.SboAddon.Framework/Helpers/SboTransaction.cs
@@ -0,0 +1,33 @@
+﻿using SAPbobsCOM;
+using System;
+
+namespace ITCO.SboAddon.Framework.Helpers
+{
+    public class SboTransaction : IDisposable
+    {
+        private readonly Company _company;
+        private bool transactionEnded;
+
+        public SboTransaction(Company company)
+        {
+            _company = company;
+
+            if (_company.InTransaction)
+                throw new Exception("Already in transation");
+
+            _company.StartTransaction();
+        }
+
+        public void Rollback()
+        {
+            _company.EndTransaction(BoWfTransOpt.wf_RollBack);
+            transactionEnded = true;
+        }
+
+        public void Dispose()
+        {
+            if (!transactionEnded)
+                _company.EndTransaction(BoWfTransOpt.wf_Commit);
+        }
+    }
+}
