9a12df27bc9a5bec31f1df087dd6dd93bebdebb7
Kenny Westermark <kenny.westermark@itcompaniet.se>
2016-01-22 11:46:24 +0100

    DocumentExtensions: AddEx

---
+++ src/ITCO.SboAddon.Framework/Extensions/DocumentExtensions.cs
@@ -0,0 +1,19 @@
+﻿using ITCO.SboAddon.Framework.Helpers;
+using SAPbobsCOM;
+
+namespace ITCO.SboAddon.Framework.Extensions
+{
+    public static class DocumentExtensions
+    {
+        /// <summary>
+        /// Add Document.
+        /// Throws Exeption if failed instead of returnCode.
+        /// </summary>
+        /// <param name="documents"></param>
+        public static void AddEx(this IDocuments documents)
+        {
+            var returnCode = documents.Add();
+            ErrorHelper.HandleErrorWithException(returnCode, "Could not Add Document");
+        }
+    }
+}
