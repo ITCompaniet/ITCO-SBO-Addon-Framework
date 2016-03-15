4137fff3ea83b97105cf7c320718ba79e5aef094
Kenny Westermark <kenny.westermark@itcompaniet.se>
2016-03-15 17:23:04 +0100

    BusinessObjectInfoExtentions

---
+++ src/ITCO.SboAddon.Framework/Extensions/BusinessObjectInfoExtentions.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml;
+using SAPbouiCOM;
+
+namespace ITCO.SboAddon.Framework.Extensions
+{
+    public static class BusinessObjectInfoExtentions
+    {
+        public static int GetDocEntry(this BusinessObjectInfo businessObjectInfo)
+        {
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(businessObjectInfo.ObjectKey);
+            return int.Parse(xmlDoc.SelectSingleNode("/DocumentParams/DocEntry").InnerText);
+        }
+    }
+}
