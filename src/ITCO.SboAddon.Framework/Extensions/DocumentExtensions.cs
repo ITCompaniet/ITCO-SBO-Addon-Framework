85ff638da87939a5815f35e2d2d7aaded0cbd90f
Kenny Westermark <kenny.westermark@itcompaniet.se>
2016-04-26 14:53:29 +0200

    AddAndLoadEx, AddEx return int

--- src/ITCO.SboAddon.Framework/Extensions/DocumentExtensions.cs
+++ src/ITCO.SboAddon.Framework/Extensions/DocumentExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using ITCO.SboAddon.Framework.Helpers;
 using SAPbobsCOM;
 
@@ -9,15 +10,30 @@ namespace ITCO.SboAddon.Framework.Extensions
         /// <summary>
         /// Add Document.
         /// Throws Exeption if failed instead of returnCode.
+        /// Returns DocEntry
         /// </summary>
         /// <param name="documents"></param>
-        /// <returns>NewObjectKey</returns>
-        public static string AddEx(this IDocuments documents)
+        /// <returns>Document Entry Key</returns>
+        public static int AddEx(this IDocuments documents)
         {
             var returnCode = documents.Add();
             ErrorHelper.HandleErrorWithException(returnCode, "Could not Add Document");
             
-            return SboApp.Company.GetNewObjectKey();
+            return int.Parse(SboApp.Company.GetNewObjectKey());
+        }
+
+        /// <summary>
+        /// Add Document and load added entry into Business Object
+        /// </summary>
+        /// <param name="documents"></param>
+        public static void AddAndLoadEx(this IDocuments documents)
+        {
+            var returnCode = documents.Add();
+            ErrorHelper.HandleErrorWithException(returnCode, "Could not Add Document");
+
+            var documentEntryKey = int.Parse(SboApp.Company.GetNewObjectKey());
+            if (!documents.GetByKey(documentEntryKey))
+                throw new Exception($"Could not load document with DocEntry {documentEntryKey}");
         }
 
         /// <summary>
