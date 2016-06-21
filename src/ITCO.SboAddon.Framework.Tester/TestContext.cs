d817cd24c584f4a48723f2d3f98adfe6428cc10f
Kenny Westermark <kenny.westermark@itcompaniet.se>
2016-06-21 21:18:45 +0200

    Unit test: Setting Service

--- src/ITCO.SboAddon.Framework.Tester/TestContext.cs
+++ src/ITCO.SboAddon.Framework.Tester/TestContext.cs
@@ -6,7 +6,12 @@ using ITCO.SboAddon.Framework.Services;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
+using System.Threading;
 using System.Windows.Forms;
+using ITCO.SboAddon.Framework.Extensions;
+using SAPbobsCOM;
+using SAPbouiCOM;
+using Application = System.Windows.Forms.Application;
 
 namespace ITCO.SboAddon.Framework.Tester
 {
@@ -20,18 +25,37 @@ namespace ITCO.SboAddon.Framework.Tester
                 if (!SboApp.ApplicationConnected)
                     throw new Exception("SboApp not Connected");
 
+
+                //Test_CopyTo();
+
                 //Test_Form();
                 //Test_MenuCreate();
                 Test_GetString();
                 //Test_Setting();
                 //Test_SettingAsk();
-   
+                //Test_FileDialogs();
+
             }
             catch (Exception e)
             {
                 MessageBox.Show($"Error: {e.Message}\nExiting...");
                 Application.Exit();
             }
+        }
+
+        private void Test_CopyTo()
+        {
+            using (var delivery = SboApp.Company.GetBusinessObject<Documents>(BoObjectTypes.oDeliveryNotes))
+            {
+                delivery.Object.GetByKey(564);
+                var invoice = delivery.Object.CopyTo(BoObjectTypes.oInvoices);
+            }
+
+        }
+
+        private void Test_FileDialogs()
+        {
+            var file = FileDialogHelper.OpenFile();
         }
 
         private void Test_SettingAsk()
@@ -82,46 +106,36 @@ namespace ITCO.SboAddon.Framework.Tester
 
         private void Test_GetString()
         {
-            var dialog = InputHelper.GetInputs("My Inputs")
-                .AddText("t1", "Title 1")
-                .AddInput(new TextDialogInput("t2", "Title 2", "def 2"))
-                .AddInput(new TextDialogInput("t3", "Title 3 Req", null, true))
-                .AddInput(new DateDialogInput("t4", "Date 1"))
-                .AddInput(new DecimalDialogInput("d1", "Decimal 1", 5.5m))
-                .AddInput(new IntegerDialogInput("i1", "Integer 1", 1))
-                .AddInput(new CheckboxDialogInput("t5", "Check On", true))
-                .AddInput(new CheckboxDialogInput("t6", "Check off", false))
-                .AddInput(new DropdownDialogInput("t7", "Dropdown 1", new Dictionary<string, string>() {
-                    { "o1", "Option 1" },
-                    { "o2", "Option 2" }
-                }, "o2", true));
-            //.Result();
 
-            var result = dialog.Result();
+            try
+            {
+                var dialog = InputHelper.GetInputs("My Inputs")
+                    .AddText("t1", "Title 1")
+                    .AddInput(new TextDialogInput("t2", "Title 2", "def 2"))
+                    .AddInput(new TextDialogInput("t3", "Title 3 Req", null, true))
+                    .AddInput(new DateDialogInput("t4", "Date 1"))
+                    .AddInput(new DecimalDialogInput("d1", "Decimal 1", 5.5m))
+                    .AddInput(new IntegerDialogInput("i1", "Integer 1", 1))
+                    .AddInput(new CheckboxDialogInput("t5", "Check On", true))
+                    .AddInput(new CheckboxDialogInput("t6", "Check off", false))
+                    .AddInput(new DropdownDialogInput("t7", "Dropdown 1", new Dictionary<string, string>()
+                    {
+                        {"o1", "Option 1"},
+                        {"o2", "Option 2"}
+                    }, "o2", true));
 
-            foreach (var o in result)
+                var result = dialog.Result();
+
+            }
+            catch (DialogCanceledException de)
             {
                 
             }
-            //var result = new STAInvoker<InputHelper, IDictionary<string, object>>
-            //    (dialog, (x) => x.Result()).Invoke();
+            catch (Exception e)
+            {
 
-            /*
-            IDictionary<string, object> result = InputHelper.GetInputs("Test", new List<IDialogInput>() {
-                new TextDialogInput("t1", "Title 1"),
-                new TextDialogInput("t2", "Title 2", "def 2"),
-                new TextDialogInput("t3", "Title 3 Req", null, true),
-                new DateDialogInput("t4", "Date 1"),
-                new DecimalDialogInput("d1", "Decimal 1", 5.5m),
-                new IntegerDialogInput("i1", "Integer 1", 1),
-                new CheckboxDialogInput("t5", "Check On", true),
-                new CheckboxDialogInput("t6", "Check off", false),
-                new DropdownDialogInput("t7", "Dropdown 1", new Dictionary<string, string>() {
-                    { "o1", "Option 1" },
-                    { "o2", "Option 2" }
-                }, "o2", true)
-            });
-            */
+                
+            }
         }
     }
 
---
+++ src/ITCO.SboAddon.Framework.Tests/SettingServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ITCO.SboAddon.Framework;
+using ITCO.SboAddon.Framework.Helpers;
+using ITCO.SboAddon.Framework.Services;
+
+namespace ITCO.SboAddon.Framework.Tests
+{
+    [TestClass]
+    public class SettingServiceTests
+    {
+        [ClassInitialize]
+        public static void Init(TestContext textContext)
+        {
+            SboApp.Connect(SboApp.DebugConnectionString);
+
+            SboRecordset.NonQuery("DELETE FROM [@ITCO_FW_SETTINGS] WHERE [Code] LIKE 'test%'");
+        }
+
+        [TestMethod]
+        public void TestNoSettings()
+        {
+            var stringValue = SettingService.GetSettingByKey<string>("test.string");
+            var intValue = SettingService.GetSettingByKey<int>("test.int");
+            var nullintValue = SettingService.GetSettingByKey<int?>("test.int");
+            var decimalValue = SettingService.GetSettingByKey<decimal>("test.decimal");
+            var datetimeValue = SettingService.GetSettingByKey<DateTime>("test.date");
+            var boolValue = SettingService.GetSettingByKey<bool>("test.bool");
+            var userStringValue = SettingService.GetCurrentUserSettingByKey<string>("test.user");
+
+            Assert.AreEqual(null, stringValue);
+            Assert.AreEqual(0, intValue);
+            Assert.AreEqual(null, nullintValue);
+            Assert.AreEqual(0m, decimalValue);
+            Assert.AreEqual(new DateTime(), datetimeValue);
+            Assert.AreEqual(false, boolValue);
+            Assert.AreEqual(null, userStringValue);
+
+        }
+    }
+}
