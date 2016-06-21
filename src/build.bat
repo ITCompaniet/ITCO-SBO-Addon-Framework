c348eb1538aec250279d96108e8cee6c5440c0b0
Kenny Westermark <kenny.westermark@itcompaniet.se>
2016-06-21 21:33:17 +0200

    build script init

---
+++ src/build.bat
@@ -0,0 +1,25 @@
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+
+set version=
+if not "%PackageVersion%" == "" (
+   set version=-Version %PackageVersion%
+)
+
+REM Build
+"%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" ITCO.SboAddon.Framework.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+if not "%errorlevel%"=="0" goto failure
+
+REM Package
+mkdir Build
+call %nuget% pack "ITCO.SboAddon.Framework\ITCO.SboAddon.Framework.csproj" -symbols -o Build -p Configuration=%config% %version%
+if not "%errorlevel%"=="0" goto failure
+
+:success
+exit 0
+
+:failure
+exit -1
