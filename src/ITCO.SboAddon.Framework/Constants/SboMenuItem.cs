50f333f1c9b3d4e32f15090f6632dcd37f8dc765
Kenny Westermark <kenny.westermark@itcompaniet.se>
2016-09-13 14:22:37 +0200

    SboMenuItem constants

--- src/ITCO.SboAddon.Framework/Constants/SboMenuItem.cs
+++ src/ITCO.SboAddon.Framework/Constants/SboMenuItem.cs
@@ -3,11 +3,13 @@ namespace ITCO.SboAddon.Framework.Constants
 {
     public class SboMenuItem
     {
+        public const string Tools = "4864";
         public const string Modules = "43520";
         public const string Finance = "1536";
         public const string Sales = "2048";
         public const string Purchase = "2304";
         public const string BusinessPartner = "43535";
+        public const string Payments = "43537";
         public const string Inventory = "3072";
     }
 }
