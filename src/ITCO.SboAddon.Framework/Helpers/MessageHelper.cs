29384e969a2421b90aa67ac0b04011cb8419b9c6
Kenny Westermark <kenny.westermark@itcompaniet.se>
2016-11-17 10:45:51 +0100

    Inverted SendMessage userCodes array handling

--- src/ITCO.SboAddon.Framework/Helpers/MessageHelper.cs
+++ src/ITCO.SboAddon.Framework/Helpers/MessageHelper.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using Extensions;
     using SAPbobsCOM;
 
@@ -15,19 +16,19 @@
         /// </summary>
         /// <param name="subject"></param>
         /// <param name="text"></param>
-        /// <param name="userCode"></param>
+        /// <param name="userCodes"></param>
         /// <param name="docNum"></param>
         /// <param name="boObjectTypes"></param>
-        public static void SendMessage(string subject, string text, string userCode,
+        public static void SendMessage(string subject, string text, IEnumerable<string> userCodes,
             int? docNum = null, BoObjectTypes? boObjectTypes = null)
         {
-            SendMessage(subject, text, new[] {userCode}, docNum, boObjectTypes);
+            userCodes.ToList().ForEach(userCode => SendMessage(subject, text, userCode, docNum, boObjectTypes));
         }
 
         /// <summary>
         /// Send internal SBO message
         /// </summary>
-        public static void SendMessage(string subject, string text, IEnumerable<string> userCodes,
+        public static void SendMessage(string subject, string text, string userCode,
             int? docNum = null, BoObjectTypes? boObjectTypes = null)
         {
             var companyService = SboApp.Company.GetCompanyService();
@@ -36,31 +37,28 @@
             var message = messageService.GetDataInterface(MessagesServiceDataInterfaces.msdiMessage) as Message;
             try
             {
-                foreach (var userCode in userCodes)
+                message.Subject = subject;
+                message.Text = text;
+                message.RecipientCollection.Add();
+                message.RecipientCollection.Item(0).SendInternal = BoYesNoEnum.tYES;
+                message.RecipientCollection.Item(0).UserCode = userCode;
+
+                if (docNum.HasValue && boObjectTypes.HasValue)
                 {
-                    message.Subject = subject;
-                    message.Text = text;
-                    message.RecipientCollection.Add();
-                    message.RecipientCollection.Item(0).SendInternal = BoYesNoEnum.tYES;
-                    message.RecipientCollection.Item(0).UserCode = userCode;
-
-                    if (docNum.HasValue && boObjectTypes.HasValue)
+                    var docEntry = docNum.Value.GetDocEntry("ORDR");
+                    if (docEntry.HasValue)
                     {
-                        var docEntry = docNum.Value.GetDocEntry("ORDR");
-                        if (docEntry.HasValue)
-                        {
-                            var column1 = message.MessageDataColumns.Add();
-                            column1.ColumnName = "Document";
-                            column1.Link = BoYesNoEnum.tYES;
+                        var column1 = message.MessageDataColumns.Add();
+                        column1.ColumnName = "Document";
+                        column1.Link = BoYesNoEnum.tYES;
 
-                            var line1 = column1.MessageDataLines.Add();
-                            line1.Value = docNum.ToString();
-                            line1.ObjectKey = docEntry.Value.ToString();
-                            line1.Object = ((int) boObjectTypes.Value).ToString();
-                        }
+                        var line1 = column1.MessageDataLines.Add();
+                        line1.Value = docNum.ToString();
+                        line1.ObjectKey = docEntry.Value.ToString();
+                        line1.Object = ((int) boObjectTypes.Value).ToString();
                     }
-                    messageService.SendMessage(message);
                 }
+                messageService.SendMessage(message);
             }
             catch (Exception e)
             {
